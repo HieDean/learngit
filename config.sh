31ea13e94a6c135f6946d1dce5edd6602fe67520
hiedean <799287043@qq.com>
2020-10-24 12:40:21 +0800

    autoUpdate

--- config.sh
+++ config.sh
@@ -1,5 +1,8 @@
 # test
+cd /home/lab532/hiedean/learngit/
 git config --global user.email 799287043@qq.com
 git config --global user.name hiedean
-python config.py
+python /home/lab532/hiedean/learngit/config.py
 git config credential.helper store
+cd ./git
+chmod 777 -R *
